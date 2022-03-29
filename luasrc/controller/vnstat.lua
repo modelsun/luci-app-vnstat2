6c039013ab0852ca26f319c31e62ea083ec5c53a
modelsun <77215718+modelsun@users.noreply.github.com>
2022-03-29 22:23:06 +0800

    Update vnstat.lua

--- luasrc/controller/vnstat.lua
+++ luasrc/controller/vnstat.lua
@@ -1,7 +1,7 @@
 module("luci.controller.vnstat", package.seeall)
 
 function index()
-	entry({"admin", "status", "vnstat流量"}, alias("admin", "network", "vnstat", "graphs"), _("VnStat流量"), 90)
-	entry({"admin", "status", "vnstat流量", "graphs"}, template("vnstat"), _("Graphs"), 1)
-	entry({"admin", "status", "vnstat流量", "config"}, cbi("vnstat"), _("Configuration"), 2)
+	entry({"admin", "status", "vnstat"}, alias("admin", "status", "vnstat", "graphs"), _("VnStat流量"), 90)
+	entry({"admin", "status", "vnstat", "graphs"}, template("vnstat"), _("Graphs"), 1)
+	entry({"admin", "status", "vnstat", "config"}, cbi("vnstat"), _("Configuration"), 2)
 end
