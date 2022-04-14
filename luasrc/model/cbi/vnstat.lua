09032fcee4d08dc7b0e47064ad27fcc29dd848e3
modelsun <77215718+modelsun@users.noreply.github.com>
2022-04-14 13:19:27 +0800

    Update vnstat.lua

--- luasrc/model/cbi/vnstat.lua
+++ luasrc/model/cbi/vnstat.lua
@@ -13,7 +13,7 @@ for line in io.lines("/etc/vnstat.conf") do
 	if dbdir then break end
 end
 
-dbdir = dbdir or "/tmp/config/vnstat" 
+dbdir = dbdir or "/etc/config/vnstat" 
 --"/var/lib/vnstat"
 
 
